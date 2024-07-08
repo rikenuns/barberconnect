1e6a8cc26aca1bce7c4ed08a168af47dc7e3341f
rikenuns <deividhenrique1020@gmail.com>
2024-07-07 23:58:10 -0300

    BarberConnet

---
+++ code/cadastro/cadastro.js
@@ -0,0 +1,8 @@
+const voltar=document.getElementById("voltar")
+
+voltar.addEventListener('click',function(e){
+    e.preventDefault()
+    window.location='/code/index.html'
+    console.log(2)
+
+})
---
+++ code/login.js
@@ -0,0 +1,6 @@
+const cadastro=document.getElementById("cadastro")
+
+cadastro.addEventListener("click",function(e){
+   e.preventDefault()
+    window.location="/code/cadastro/cadastro.html"
+})
