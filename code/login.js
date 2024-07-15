7e52f0d17c791eaa19b276bac83dfd949f4c0888
rikenuns <deividhenrique1020@gmail.com>
2024-07-15 05:20:47 -0300

    nome da alteração

--- code/login.js
+++ code/login.js
@@ -1,6 +1,12 @@
 const cadastro=document.getElementById("cadastro")
+const entrar=document.getElementById("entrar")
 
 cadastro.addEventListener("click",function(e){
    e.preventDefault()
     window.location="/code/cadastro/cadastro.html"
 })
+
+/* entrar.addEventListener("click",function(e){
+   e.preventDefault()
+   window.location="/code/home/home.html"
+}) */
